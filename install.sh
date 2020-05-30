22dd968a6db89b393830d501896790339b80dd44
Roberto Arcomano <bertolinux@gmail.com>
2020-05-30 16:16:07 +0100

    changed pwd

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 # Script to Create a Container for LAMP, using Docker
 
 # 1. Download git repository
+rm -rf /tmp/wordpress
 git clone https://github.com/robertoarcomano/wordpress.git /tmp/wordpress
-cd /tmp/wordpress
 
 # 2. Download Dockerfile and create image
 docker rmi -f wordpress
-docker build -t wordpress .
+docker build -t wordpress /tmp/wordpress
 
 # 3. Create the container from the image
 docker rm -f wordpress
@@ -16,6 +16,3 @@ docker create -p 81:80 --name "wordpress" wordpress
 # 4. Start the container
 docker start wordpress
 
-# 5. Delete supervisor.conf /tmp/wordpress
-cd 
-rm -rf /tmp/wordpress

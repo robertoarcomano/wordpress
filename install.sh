35b6904d210e3bd66eae26d9ce34a707b9993f59
Roberto Arcomano <bertolinux@gmail.com>
2020-05-30 16:04:16 +0100

    changed name

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 # Script to Create a Container for LAMP, using Docker
 
 # 1. Download git repository
-git clone https://github.com/robertoarcomano/DockerLemp.git /tmp/DockerLemp
-cd /tmp/DockerLemp
+git clone https://github.com/robertoarcomano/wordpress.git /tmp/wordpress
+cd /tmp/wordpress
 
 # 2. Download Dockerfile and create image
-docker build -t dockerlemp .
+docker build -t wordpress .
 
 # 3. Create the container from the image
-docker create -p 81:80 --name "dockerlemp" dockerlemp
+docker create -p 81:80 --name "wordpress" wordpress
 
 # 4. Start the container
-docker start dockerlemp
+docker start wordpress
 
-# 5. Delete supervisor.conf /tmp/Dockerfile
+# 5. Delete supervisor.conf /tmp/wordpress
 cd 
-rm -rf /tmp/DockerLemp
+rm -rf /tmp/wordpress
